bf12f1dd98bcbb5b57d003bb15fb30dcdb98c512
wmzn-ltpp <1491579574@qq.com>
2024-03-08 19:27:15 +0800

    feat:update

--- app/controller/Index.php
+++ app/controller/Index.php
@@ -28,7 +28,7 @@ class Index
             return json($check_safe_json);
         }
         if (!Base::judgeJudgeInstall()) {
-            return json(['code' => -1, 'data' => '判题机未安装', 'usememory' => 0, 'usetime' => 0]);
+            return json(['code' => -1, 'data' => '判题机未安装', 'memory' => 0, 'time' => 0]);
         }
         $run_res =  $this->run($code, $userlanguage, $testin);
         return json($run_res);
@@ -94,7 +94,7 @@ class Index
             Base::removeBr($err_data);
             $code .= $err_data;
             $res_data .= $err_data;
-            return ['code' => -1, 'data' => $res_data, 'usememory' => 0, 'usetime' => 0];
+            return ['code' => -1, 'data' => $res_data, 'memory' => 0, 'time' => 0];
         }
 
         $out = [];
@@ -103,12 +103,12 @@ class Index
         $out = Base::run($userlanguage, $filepath, $inpath, $outpath, $errpath, $runcodefilepath, $limittime, $limitmemory);
 
         if (!$out || empty($out)) {
-            return ['code' => -1, 'data' => '判题机运行异常！', 'usememory' => 0, 'usetime' => 0];
+            return ['code' => -1, 'data' => '判题机运行异常！', 'memory' => 0, 'time' => 0];
         }
         $out = $out[0];
         $run_resource_consumption = Base::getCodeTimeMemory($out);
         if (!$run_resource_consumption || !isset($run_resource_consumption['status'])) {
-            return ['code' => -1, 'data' => '判题机运行异常！', 'usememory' => 0, 'usetime' => 0];
+            return ['code' => -1, 'data' => '判题机运行异常！', 'memory' => 0, 'time' => 0];
         }
 
         $status = $run_resource_consumption['status'] ?? 0;
@@ -117,7 +117,7 @@ class Index
 
         if ($status == Base::$judge_server_error) {
             $msg = $run_resource_consumption['msg'];
-            return ['code' => -1, 'data' => '判题机运行异常！' . "\n" . $msg, 'usememory' => 0, 'usetime' => 0];
+            return ['code' => -1, 'data' => '判题机运行异常！' . "\n" . $msg, 'memory' => 0, 'time' => 0];
         }
 
         if ($status == Base::$judge_code_error) {
@@ -141,8 +141,8 @@ class Index
             return [
                 'code' => -1,
                 'data' => $err_data,
-                'usetime' => $time_used,
-                'usememory' => $memory_used
+                'time' => $time_used,
+                'memory' => $memory_used
             ];
         }
 
@@ -161,17 +161,17 @@ class Index
         switch ($status) {
             case Base::$judge_code_tle:
                 Base::deleteAllFile($filepath);
-                return ['code' => -1, 'data' => 'TLE！' . '请更改代码后再次尝试哦！' . ($resout ? "\n" . $resout : ''), 'usetime' => $time_used, 'usememory' => $memory_used];
+                return ['code' => -1, 'data' => 'TLE！' . '请更改代码后再次尝试哦！' . ($resout ? "\n" . $resout : ''), 'time' => $time_used, 'memory' => $memory_used];
             case Base::$judge_code_mle:
                 Base::deleteAllFile($filepath);
-                return ['code' => -1, 'data' => 'MLE！' . '请更改代码后再次尝试哦！' . ($resout ? "\n" . $resout : ''), 'usetime' => $time_used, 'usememory' => $memory_used];
+                return ['code' => -1, 'data' => 'MLE！' . '请更改代码后再次尝试哦！' . ($resout ? "\n" . $resout : ''), 'time' => $time_used, 'memory' => $memory_used];
             case Base::$judge_code_re:
                 Base::deleteAllFile($filepath);
-                return ['code' => -1, 'data' => 'RE！' . '请更改代码后再次尝试哦！' . ($resout ? "\n" . $resout : ''), 'usetime' => $time_used, 'usememory' => $memory_used];
+                return ['code' => -1, 'data' => 'RE！' . '请更改代码后再次尝试哦！' . ($resout ? "\n" . $resout : ''), 'time' => $time_used, 'memory' => $memory_used];
             default:
                 break;
         }
         Base::deleteAllFile($filepath);
-        return ['code' => 1, 'data' => $resout, 'usetime' => $time_used, 'usememory' => $memory_used];
+        return ['code' => 1, 'data' => $resout, 'time' => $time_used, 'memory' => $memory_used];
     }
 }
