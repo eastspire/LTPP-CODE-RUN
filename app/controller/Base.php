359884b16d61e8eb04c65e87ad6ec76ba19819a7
wmzn-ltpp <1491579574@qq.com>
2024-03-21 08:37:24 +0800

    feat:update

--- app/controller/Base.php
+++ app/controller/Base.php
@@ -206,9 +206,9 @@ class Base
 
     /**
      * 代码输出内容长度限制
-     * @var int $code_out_limit 文章内容长度限制
+     * @var int $code_out_limit 代码输出内容长度限制
      */
-    static $code_out_limit = 10000;
+    static $code_out_limit = 100000;
 
     /**
      * LTPP文件夹绝对路径
