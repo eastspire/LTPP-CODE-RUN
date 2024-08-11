d3791bf319a686e364ddd20c0bca9605b5a9d335
尤雨东 <root@ltpp.vip>
2024-08-11 23:26:23 +0800

    feat:update

--- sh/init.sh
+++ sh/init.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 git remote add ltpp ssh://git@git.ltpp.vip:66/root/ltpp-code-run.git;
 git remote add github git@github.com:ltpp-universe/LTPP-CODE-RUN.git;
+git remote add origin git@github.com:ltpp-universe/LTPP-CODE-RUN.git;
